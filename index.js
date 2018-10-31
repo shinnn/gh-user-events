a45edd690196ce5fa9f0b70edb87cd13151eb036
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-10-31 19:55:50 +0900

    t

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-/*!
- * gh-user-events | MIT (c) Shinnosuke Watanabe
- * https://github.com/shinnn/gh-user-events
-*/
 'use strict';
 
-const arrFilter = require('arr-filter');
-const arrReduce = require('arr-reduce');
-const filledArray = require('filled-array');
 const ghGet = require('gh-get');
 const isNaturalNumber = require('is-natural-number');
 
@@ -14,60 +7,51 @@ const maxPageSizeErrMsg = '`maxPageSize` option must be a number of pages ' +
                           'no fewer than 1 and no greater than 10. ' +
                           '(One page includes 30 items. https://developer.github.com/v3/#pagination)';
 
-module.exports = function ghUserEvents(user, options) {
-  if (typeof user !== 'string') {
-    return Promise.reject(new TypeError(
-      `${user} is not a string. Expected a Github username to get the events performed by the account.`
-    ));
-  }
+module.exports = async function ghUserEvents(user, options) {
+	if (typeof user !== 'string') {
+		throw new TypeError(`${
+			user
+		} is not a string. Expected a Github username to get the events performed by the account.`));
+	}
 
-  if (user === '') {
-    return Promise.reject(new Error(
-      'Expected a Github username to get the events performed by the account, but received an empty string.'
-    ));
-  }
+	if (user.length === 0) {
+		return Promise.reject(new Error(
+			'Expected a Github username to get the events performed by the account, but received an empty string.'
+		));
+	}
 
-  options = Object.assign({
-    publicOnly: false,
-    verbose: false,
-    maxPageSize: 10
-  }, options);
+	options = {
+		verbose: false,
+		userAgent: 'https://github.com/shinnn/github-user-events'
+		...options
+	};
 
-  if (typeof options.publicOnly !== 'boolean') {
-    return Promise.reject(new TypeError(
-      String(options.publicOnly) +
-      ' is not a Boolean value. `publicOnly` option must be a Boolean value.' +
-      ' (`false` by default)'
-    ));
-  }
+	if (options.publicOnly !== undefined && typeof options.publicOnly !== 'boolean') {
+		throw new TypeError(
+			String(options.publicOnly) +
+			' is not a Boolean value. `publicOnly` option must be a Boolean value.' +
+			' (`false` by default)'
+		);
+	}
 
-  if (options.maxPageSize <= 0) {
-    return Promise.reject(new TypeError(`Expected a positive number but recieved ${options.maxPageSize}. ${maxPageSizeErrMsg}`));
-  }
+	if (options.publicOnly !== undefined) {
+		if (!isNaturalNumber(options.maxPageSize)) {
+			throw new TypeError(`${options.maxPageSize} is not a natural number. ${maxPageSizeErrMsg}`);
+		}
 
-  if (options.maxPageSize > 10) {
-    return Promise.reject(new TypeError(`${options.maxPageSize} exceeds the limit of \`maxPageSize\` option. ${maxPageSizeErrMsg}`));
-  }
+		if (options.maxPageSize <= 0) {
+			throw new TypeError(`Expected a positive number but recieved ${options.maxPageSize}. ${maxPageSizeErrMsg}`);
+		}
 
-  if (!isNaturalNumber(options.maxPageSize)) {
-    return Promise.reject(new TypeError(`${options.maxPageSize} is not a natural number. ${maxPageSizeErrMsg}`));
-  }
+		if (options.maxPageSize > 10) {
+			throw new TypeError(`${options.maxPageSize} exceeds the limit of \`maxPageSize\` option. ${maxPageSizeErrMsg}`);
+		}
+	}
 
-  options.headers = Object.assign({
-    'user-agent': 'https://github.com/shinnn/github-user-events'
-  }, options.headers);
+	const path = `users/${user}/events${options.publicOnly ? '/public' : ''}`;
 
-  const urlPath = `users/${user}/events${options.publicOnly ? '/public' : ''}`;
-
-  return Promise.all(filledArray(function createRequestUrl(index) {
-    return ghGet(`${urlPath}?page=${index + 1}`, options);
-  }, options.maxPageSize))
-  .then(function joinUserEvents(responses) {
-    return Promise.resolve(arrReduce(responses, (arr, response) => {
-      arr.push(...arrFilter(response.body, function removeDuplicates(event) {
-        return !arr.some(el => el.id === event.id);
-      }));
-      return arr;
-    }, []));
-  });
+	const responses await Promise.all(Array.from({length: options.maxPageSize || 10}, (value, index) => ghGet(`${urlPath}?page=${index + 1}`, options)));
+	responses.reduce((arr, response) => {
+		arr.push(response.body.filter(event => !arr.some(({id}) => id === event.id)));
+	}, []);
 };
